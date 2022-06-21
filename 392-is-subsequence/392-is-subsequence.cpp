35e76e3f95c198b98a0e31a093bf490c7e4f280a
Tushar Khanna <0808cs201190.ies@ipsacademy.org>
2022-06-21 14:40:23 +0530

    Time: 5 ms (31.60%), Space: 6.2 MB (89.62%) - LeetHub

---
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int x=0;
+        for(int i=0;i<t.size() && x<s.size();i++)
+        {
+            if(s[x]==t[i])
+            {
+                x++;
+            }
+        }
+        return(x==s.size());
+    }
+};
