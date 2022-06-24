909036f85f152fc02556c75794c89189e0a663b3
Tushar Khanna <0808cs201190.ies@ipsacademy.org>
2022-06-24 23:36:08 +0530

    Added solution - LeetHub

---
+++ Is Binary Number Multiple of 3 - GFG/is-binary-number-multiple-of-3.py
@@ -0,0 +1,20 @@
+#User function Template for python3
+class Solution:
+	def isDivisible(self, s):
+		# code here
+		s=int(s,2)
+		return(int(bool(s%3==0)))
+		
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+	T=int(input())
+	for i in range(T):
+		s = input()
+		ob = Solution()
+		ans = ob.isDivisible(s)
+		print(ans)
+
+# } Driver Code Ends
