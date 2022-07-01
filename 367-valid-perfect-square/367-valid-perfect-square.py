c6b1aa11f52ae6d9b7388139dfb9d135d60c3fd3
Tushar Khanna <0808cs201190.ies@ipsacademy.org>
2022-07-01 23:47:15 +0530

    Time: 58 ms (23.65%), Space: 13.8 MB (96.34%) - LeetHub

--- 367-valid-perfect-square/367-valid-perfect-square.py
+++ 367-valid-perfect-square/367-valid-perfect-square.py
@@ -1,7 +1,4 @@
 import math
 class Solution:
     def isPerfectSquare(self, n: int) -> bool:
-        if (math.ceil(math.sqrt(n)) == math.floor(math.sqrt(n))):
-            return True
-        else:
-            return False
+        return(math.ceil(math.sqrt(n)) == math.floor(math.sqrt(n)))
