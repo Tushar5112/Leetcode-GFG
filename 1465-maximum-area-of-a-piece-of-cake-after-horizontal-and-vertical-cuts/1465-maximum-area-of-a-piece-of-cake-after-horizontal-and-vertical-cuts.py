d1a691dc65bd9f48898d5376d469648283ff08c3
Tushar Khanna <0808cs201190.ies@ipsacademy.org>
2022-07-02 11:59:08 +0530

    Time: 562 ms (22.01%), Space: 26.8 MB (62.62%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.py
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.py
@@ -1,18 +1,14 @@
-class Solution {
-public:
-    int maxdiff(vector<int>v, int length) {
-        sort(v.begin(),v.end());
-        int max_diff=v[0];
-        for(int i=1;i<v.size();i++) {
-            max_diff=max(max_diff,v[i]-v[i-1]);
-        }
-        max_diff=max(max_diff,length-v.back());
-        return max_diff;
-    }
-    int maxArea(int h, int w, vector<int>& horizontalCuts, vector<int>& verticalCuts) {
-        int mod=1e9+7;
-        long max_h=maxdiff(horizontalCuts,h);
-        long max_v=maxdiff(verticalCuts,w);
-        return((max_h*max_v)%mod);
-    }
-};
+class Solution:
+    def maxArea(self, h: int, w: int, horizontalCuts: List[int], verticalCuts: List[int]) -> int:
+        mod = int(1e9 + 7)
+        horizontalCuts.sort()
+        verticalCuts.sort()
+        horizontalCuts.append(h)
+        verticalCuts.append(w)
+        mh = horizontalCuts[0]
+        mv = verticalCuts[0]
+        for i in range(len(horizontalCuts) - 1):
+            mh = max(mh, horizontalCuts[i+1] - horizontalCuts[i]) 
+        for i in range(len(verticalCuts) - 1):
+            mv = max(mv, verticalCuts[i+1] - verticalCuts[i])
+        return (mh*mv)%mod
