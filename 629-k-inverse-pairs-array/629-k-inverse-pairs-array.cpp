1853ff55b343b8b41d97ed88842c7ed83829ee29
Tushar Khanna <0808cs201190.ies@ipsacademy.org>
2022-07-17 16:43:24 +0530

    Time: 45 ms (66.67%), Space: 10.3 MB (83.33%) - LeetHub

---
+++ 629-k-inverse-pairs-array/629-k-inverse-pairs-array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+int mod=1000000007;
+int f[1001][1001] = {};
+public:
+    int kInversePairs(int n, int K) {
+        f[0][0] = 1;
+        for (int i = 1; i <= n; ++ i) 
+        {
+            long long s = 0; 
+            for (int j = 0; j <= K; ++ j)
+            {
+                s += f[i - 1][j];
+                if (j >= i)
+                    s -= f[i - 1][j - i];
+                f[i][j] = ((long long)f[i][j] + s) % mod; 
+            }
+        }
+        return f[n][K];    
+    }
+};
